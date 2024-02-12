fc9c409ead77ad2fc0a06eed668f5dd8d5911a2d
Herman Zhukov <societyofbruh@gmail.com>
2024-02-12 20:42:18 +0300

    Move function definitions out of the header into the source file, add conditional internal linkage, move auxiliary I/O out of the library, add bits macro

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -2,7 +2,7 @@
 
 // TASK FUNCTIONS
 // WIP
-int s21_add(s21_decimal v1, s21_decimal v2, s21_decimal *result) {}
+int s21_add(s21_decimal v1, s21_decimal v2, s21_decimal *result) { return 0; }
 // TODO
 // int s21_sub(s21_decimal v1, s21_decimal v2, s21_decimal *result) {}
 // int s21_mul(s21_decimal v1, s21_decimal v2, s21_decimal *result) {}
@@ -26,3 +26,52 @@ int s21_add(s21_decimal v1, s21_decimal v2, s21_decimal *result) {}
 // int s21_negate(s21_decimal value, s21_decimal *result) {}
 
 // OTHER FUNCTIONS
+
+S21_STATIC_KEYWORD unsigned int s21_get_bit(const s21_decimal value,
+                                            int bit_offset) {
+  unsigned int data_unit_size = sizeof(value.uint_data[0]) << 3,
+               rval = 0xDEADBEEF;
+  if (bit_offset < S21_DECIMAL_SIZE_IN_BITS && bit_offset >= 0)
+    rval = (value.uint_data[bit_offset / data_unit_size] >>
+            (bit_offset % data_unit_size)) &
+           1;
+  else
+    rval = bit_offset;
+  return rval;
+}
+
+S21_STATIC_KEYWORD unsigned int s21_set_bit(s21_decimal *value,
+                                            int bit_offset) {
+  unsigned int data_unit_size = sizeof(value->uint_data[0]) << 3, rval = 0;
+  if (bit_offset < S21_DECIMAL_SIZE_IN_BITS && bit_offset >= 0) {
+    rval = 1;
+    value->uint_data[bit_offset / data_unit_size] |=
+        (1 << (bit_offset % data_unit_size));
+  } else
+    rval = bit_offset;
+  return rval;
+}
+
+S21_STATIC_KEYWORD unsigned int s21_clear_bit(s21_decimal *value,
+                                              int bit_offset) {
+  unsigned int data_unit_size = sizeof(value->uint_data[0]) << 3, rval = 1;
+  if (bit_offset < S21_DECIMAL_SIZE_IN_BITS && bit_offset >= 0) {
+    rval = 0;
+    value->uint_data[bit_offset / data_unit_size] &=
+        ~(1 << (bit_offset % data_unit_size));
+  } else
+    rval = bit_offset;
+  return rval;
+}
+
+S21_STATIC_KEYWORD unsigned int s21_toggle_bit(s21_decimal *value,
+                                               int bit_offset) {
+  unsigned int data_unit_size = sizeof(value->uint_data[0]) << 3,
+               rval = 0xDEADBEEF;
+  if (bit_offset < S21_DECIMAL_SIZE_IN_BITS && bit_offset >= 0)
+    rval = value->uint_data[bit_offset / data_unit_size] ^=
+        (1 << (bit_offset % data_unit_size));
+  else
+    rval = bit_offset;
+  return rval;
+}
