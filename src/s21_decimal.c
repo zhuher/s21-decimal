792e37506ae4b27fb330cd72b7c9b028d980a0ef
Herman Zhukov <liannele@student.21-school.ru>
2024-02-13 02:35:17 +0300

    Add auxiliary bit shifting functions, work on addition

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -2,7 +2,19 @@
 
 // TASK FUNCTIONS
 // WIP
-int s21_add(s21_decimal v1, s21_decimal v2, s21_decimal *result) { return 0; }
+int s21_add(s21_decimal v1, s21_decimal v2, s21_decimal *result) {
+  unsigned int carry = 0, rval = OK;
+  for (int i = 0; i < S21_DECIMAL_SIZE_IN_INTS; ++i) {
+    unsigned int temp = v1.uint_data[i] + v2.uint_data[i] + carry;
+    carry = temp < v1.uint_data[i] || temp < v2.uint_data[i];
+    result->uint_data[i] = temp;
+    if (__s21_read_bit(*result, (S21_DECIMAL_SIZE_IN_INTS - 1) << 5)) {
+      rval = TOO_BIG;
+      break;
+    }
+  }
+  return rval;
+}
 // TODO
 // int s21_sub(s21_decimal v1, s21_decimal v2, s21_decimal *result) {}
 // int s21_mul(s21_decimal v1, s21_decimal v2, s21_decimal *result) {}
@@ -27,51 +39,180 @@ int s21_add(s21_decimal v1, s21_decimal v2, s21_decimal *result) { return 0; }
 
 // OTHER FUNCTIONS
 
-S21_STATIC_KEYWORD unsigned int s21_get_bit(const s21_decimal value,
-                                            int bit_offset) {
-  unsigned int data_unit_size = sizeof(value.uint_data[0]) << 3,
-               rval = 0xDEADBEEF;
-  if (bit_offset < S21_DECIMAL_SIZE_IN_BITS && bit_offset >= 0)
-    rval = (value.uint_data[bit_offset / data_unit_size] >>
-            (bit_offset % data_unit_size)) &
-           1;
-  else
-    rval = bit_offset;
+// Function to initialize(and memset) a decimal to 0
+s21_decimal s21_decimal_init(void) {
+  // Initialize the return value
+  s21_decimal rval;
+  // Memset all the bytes to 0
+  for (int i = 0; i < sizeof(s21_decimal); ++i) {
+    ((char *)&rval)[i] = 0;
+  }
+  // Return the return value
+  return rval;
+};
+
+// Function to shift a decimal to the left by one
+S21_STATIC_KEYWORD unsigned int __s21_left_shift_one(s21_decimal value,
+                                                     s21_decimal *result) {
+  // Initialize the carry and the return value
+  unsigned int carry = 0, rval = OK;
+  // Loop through all but the last data unit in reverse order
+  for (int i = S21_DECIMAL_SIZE_IN_INTS - 2; i >= 0; --i) {
+    // Check if the result is too big
+    if (__s21_read_bit(*result, 96)) {
+      // Set the return value to TOO_BIG and break the loop
+      rval = TOO_BIG;
+      break;
+    }
+    // Shift the value to the left by one, accounting for the carry
+    unsigned int temp = value.uint_data[i] << 1 | carry;
+    // Update the carry
+    carry = value.uint_data[i] >> 31;
+    // Update the result
+    result->uint_data[i] = temp;
+  }
+  // Return the return value
   return rval;
 }
 
-S21_STATIC_KEYWORD unsigned int s21_set_bit(s21_decimal *value,
-                                            int bit_offset) {
-  unsigned int data_unit_size = sizeof(value->uint_data[0]) << 3, rval = 0;
+// Function to shift a decimal to the left by a given amount
+S21_STATIC_KEYWORD unsigned int __s21_left_shift(s21_decimal value,
+                                                 unsigned int shift,
+                                                 s21_decimal *result) {
+  // Initialize the return value
+  unsigned int rval = OK;
+  // Shift the value to the left while the shift is not 0 and the return value
+  // is OK
+  while (shift-- && !rval) {
+    // Shift the value to the left by one
+    rval = __s21_left_shift_one(value, result);
+    // Update the value
+    value = *result;
+  }
+  // Return the return value
+  return rval;
+}
+
+S21_STATIC_KEYWORD unsigned int __s21_right_shift_one(s21_decimal value,
+                                                      s21_decimal *result) {
+  // Initialize the carry and the return value
+  unsigned int carry = 0, rval = OK;
+  // Loop through all but the last data unit
+  for (int i = 0; i < S21_DECIMAL_SIZE_IN_INTS - 1; ++i) {
+    // Shift the value to the right by one, accounting for the carry from the
+    // previous iteration
+    unsigned int temp = value.uint_data[i] >> 1 | carry;
+    // Update the carry
+    carry = (value.uint_data[i] & 1) << 31;
+    // Update the result
+    result->uint_data[i] = temp;
+  }
+  // Return the return value
+  return rval;
+}
+
+S21_STATIC_KEYWORD unsigned int __s21_right_shift(s21_decimal value,
+                                                  unsigned int shift,
+                                                  s21_decimal *result) {
+  // Initialize the return value
+  unsigned int rval = OK;
+  // Shift the value to the right while the shift is not 0 and the return value
+  // is OK
+  while (shift-- && rval == OK) {
+    // Shift the value to the right by one
+    rval = __s21_right_shift_one(value, result);
+    // Update the value
+    value = *result;
+  }
+  // Return the return value
+  return rval;
+}
+
+S21_STATIC_KEYWORD
+unsigned int __s21_read_bits(const s21_decimal value, unsigned int bit_offset,
+                             unsigned int bit_count) {
+  // Initialize the return value and the size of the data unit
+  unsigned int rval = OK;
+  // Check if the bit offset and the bit count are valid
+  if (bit_offset < S21_DECIMAL_SIZE_IN_BITS && bit_offset >= 0 &&
+      bit_count > 0 && bit_count <= S21_DECIMAL_SIZE_IN_BITS) {
+    // Calculate the unit and the offset
+    unsigned int unit = bit_offset / S21_DECIMAL_UNIT_SIZE_IN_BITS,
+                 offset = bit_offset % S21_DECIMAL_UNIT_SIZE_IN_BITS;
+    // Check if the bits are in the same data unit
+    // If they are, then the return value is the bits from the offset
+    if (offset / S21_DECIMAL_UNIT_SIZE_IN_BITS ==
+        (offset + bit_count - 1) / S21_DECIMAL_UNIT_SIZE_IN_BITS) {
+      // Operation explained:
+      // 1. Get the bits from the offset
+      // 2. Return the result
+      rval = (value.uint_data[unit] >> offset) & ((1 << bit_count) - 1);
+    }
+    // If they are not, then the return value is the bits from the offset, and
+    // some bits from the next data unit
+    else {
+      // Operation explained:
+      // 1. Get the bits from the offset
+      // 2. Get the bits from the next data unit
+      // 3. Combine the bits from the offset and the bits from the next data
+      // unit
+      // 4. Return the result
+      rval =
+          (value.uint_data[unit] >> offset) |
+          ((value.uint_data[unit + 1] &
+            ((1 << (bit_count - (S21_DECIMAL_UNIT_SIZE_IN_BITS - offset))) - 1))
+           << (S21_DECIMAL_UNIT_SIZE_IN_BITS - offset));
+    }
+  }
+  return rval;
+}
+
+S21_STATIC_KEYWORD int __s21_write_bits(s21_decimal *value, unsigned int data,
+                                        unsigned int bit_offset,
+                                        unsigned int bit_count) {
+  // initialize S21_DECIMAL_UNIT_SIZE_IN_BITS
+  unsigned int rval = -bit_offset;
+  // check if bit_offset is within the range of the decimal
+  if (bit_offset < S21_DECIMAL_SIZE_IN_BITS && bit_offset >= 0 &&
+      bit_count > 0 && bit_count <= S21_DECIMAL_SIZE_IN_BITS) {
+    // calculate the unit and offset of the bit_offset
+    unsigned int unit = bit_offset / S21_DECIMAL_UNIT_SIZE_IN_BITS,
+                 offset = bit_offset % S21_DECIMAL_UNIT_SIZE_IN_BITS;
+    // check if the bits to be written are within the same unit
+    // if the bits to be written are within the same unit
+    if (offset / S21_DECIMAL_UNIT_SIZE_IN_BITS ==
+        (offset + bit_count - 1) / S21_DECIMAL_UNIT_SIZE_IN_BITS) {
+      // clear the bits to be written
+      value->uint_data[unit] &= ~(((1 << bit_count) - 1) << offset);
+      // write the bits to the decimal
+      value->uint_data[unit] |= (data << offset);
+    }
+    // if the bits to be written are not within the same unit
+    else {
+      // clear the bits of the first unit to be written
+      value->uint_data[unit] &=
+          ~((1 << (S21_DECIMAL_UNIT_SIZE_IN_BITS - offset)) - 1);
+      // write the bits to the first unit
+      value->uint_data[unit] |= (data << offset);
+      // clear the bits of the second unit to be written
+      value->uint_data[unit + 1] &=
+          ~((1 << (bit_count - (S21_DECIMAL_UNIT_SIZE_IN_BITS - offset))) - 1);
+      // write the bits to the second unit
+      value->uint_data[unit + 1] |=
+          (data >> (S21_DECIMAL_UNIT_SIZE_IN_BITS - offset));
+    }
+    rval = OK;
+  }
+  return rval;
+}
+
+S21_STATIC_KEYWORD int __s21_toggle_bit(s21_decimal *value,
+                                        unsigned int bit_offset) {
+  unsigned int rval = -bit_offset;
   if (bit_offset < S21_DECIMAL_SIZE_IN_BITS && bit_offset >= 0) {
-    rval = 1;
-    value->uint_data[bit_offset / data_unit_size] |=
-        (1 << (bit_offset % data_unit_size));
-  } else
-    rval = bit_offset;
-  return rval;
-}
-
-S21_STATIC_KEYWORD unsigned int s21_clear_bit(s21_decimal *value,
-                                              int bit_offset) {
-  unsigned int data_unit_size = sizeof(value->uint_data[0]) << 3, rval = 1;
-  if (bit_offset < S21_DECIMAL_SIZE_IN_BITS && bit_offset >= 0) {
-    rval = 0;
-    value->uint_data[bit_offset / data_unit_size] &=
-        ~(1 << (bit_offset % data_unit_size));
-  } else
-    rval = bit_offset;
-  return rval;
-}
-
-S21_STATIC_KEYWORD unsigned int s21_toggle_bit(s21_decimal *value,
-                                               int bit_offset) {
-  unsigned int data_unit_size = sizeof(value->uint_data[0]) << 3,
-               rval = 0xDEADBEEF;
-  if (bit_offset < S21_DECIMAL_SIZE_IN_BITS && bit_offset >= 0)
-    rval = value->uint_data[bit_offset / data_unit_size] ^=
-        (1 << (bit_offset % data_unit_size));
-  else
-    rval = bit_offset;
+    value->uint_data[bit_offset / S21_DECIMAL_UNIT_SIZE_IN_BITS] ^=
+        (1 << (bit_offset % S21_DECIMAL_UNIT_SIZE_IN_BITS));
+    rval = OK;
+  }
   return rval;
 }
--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -5,6 +5,13 @@
 #define S21_DECIMAL_SIZE_IN_INTS 4
 #endif
 
+#ifndef S21_DECIMAL_TEST  // We internally link some functions, unless we are
+                          // testing
+#define S21_STATIC_KEYWORD static inline
+#else
+#define S21_STATIC_KEYWORD
+#endif
+
 typedef struct {
   union {
     unsigned int uint_data[S21_DECIMAL_SIZE_IN_INTS];
@@ -14,16 +21,17 @@ typedef struct {
 } s21_decimal;
 
 #define S21_DECIMAL_SIZE_IN_BITS (sizeof(s21_decimal) << 3)
+#define S21_DECIMAL_UNIT_SIZE_IN_BITS (sizeof(int) << 3)
 
 // ERROR CODES
-#define FALSE 0;
-#define TRUE 1;
-#define OK 0;
-#define TOO_BIG 1;
-#define TOO_SMALL 2;
-#define ZERO_DIVIDING 3;
-#define CONVERT_ERROR 1;
-#define CALC_ERROR 1;
+#define FALSE 0
+#define TRUE 1
+#define OK 0
+#define TOO_BIG 1
+#define TOO_SMALL 2
+#define ZERO_DIVIDING 3
+#define CONVERT_ERROR 1
+#define CALC_ERROR 1
 
 // TASK FUNCTIONS
 int s21_add(s21_decimal v1, s21_decimal v2, s21_decimal *result);
@@ -50,18 +58,50 @@ int s21_negate(s21_decimal value, s21_decimal *result);
 
 // OTHER FUNCTIONS
 
-#ifndef S21_DECIMAL_TEST  // We internally link, unless we are testing
-#define S21_STATIC_KEYWORD static inline
-#else
-#define S21_STATIC_KEYWORD
-#endif
+S21_STATIC_KEYWORD unsigned int __s21_left_shift_one(s21_decimal value,
+                                                     s21_decimal *result);
 
-S21_STATIC_KEYWORD unsigned int s21_get_bit(const s21_decimal value,
-                                            int bit_offset);
-S21_STATIC_KEYWORD unsigned int s21_set_bit(s21_decimal *value, int bit_offset);
-S21_STATIC_KEYWORD unsigned int s21_clear_bit(s21_decimal *value,
-                                              int bit_offset);
-S21_STATIC_KEYWORD unsigned int s21_toggle_bit(s21_decimal *value,
-                                               int bit_offset);
+S21_STATIC_KEYWORD unsigned int __s21_left_shift(s21_decimal value,
+                                                 unsigned int shift,
+                                                 s21_decimal *result);
+
+S21_STATIC_KEYWORD unsigned int __s21_right_shift_one(s21_decimal value,
+                                                      s21_decimal *result);
+
+S21_STATIC_KEYWORD unsigned int __s21_right_shift(s21_decimal value,
+                                                  unsigned int shift,
+                                                  s21_decimal *result);
+
+s21_decimal s21_decimal_init(void);
+
+S21_STATIC_KEYWORD unsigned int __s21_read_bits(const s21_decimal value,
+                                                unsigned int bit_offset,
+                                                unsigned int bit_count);
+
+#define __s21_read_bit(value, bit_offset) __s21_read_bits(value, bit_offset, 1)
+
+// Exponent is 1 byte wide, located in the second most significant byte
+#define __s21_read_exponent(value) \
+  __s21_read_bits(value, S21_DECIMAL_SIZE_IN_BITS - 16, 8)
+
+// Sign is the most significant bit
+#define __s21_read_sign(value) \
+  __s21_read_bits(value, S21_DECIMAL_SIZE_IN_BITS - 1, 1)
+
+S21_STATIC_KEYWORD int __s21_write_bits(s21_decimal *value, unsigned int data,
+                                        unsigned int bit_offset,
+                                        unsigned int bit_count);
+
+#define __s21_write_exponent(value, data) \
+  __s21_write_bits(value, data, S21_DECIMAL_SIZE_IN_BITS - 16, 8)
+
+#define __s21_write_bit(value, bit_offset, data) \
+  __s21_write_bits(value, data, bit_offset, 1)
+
+// Return values:
+// 0 - bit toggled successfully
+// -bit_offset - bit_offset is too big
+S21_STATIC_KEYWORD int __s21_toggle_bit(s21_decimal *value,
+                                        unsigned int bit_offset);
 
 #endif  // S21_DECIMAL_H
